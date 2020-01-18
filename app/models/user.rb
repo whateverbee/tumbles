c64c82543ddd2e4cd4372967900487c54c6843f7
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-18 14:06:42 -0500

    adds subscriptions, join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :subscriptions, foreign_key: :follower_id,
+                           dependent: :destroy 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_190223) do
+ActiveRecord::Schema.define(version: 2020_01_18_190338) do
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "leader_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_subscriptions_on_follower_id"
+    t.index ["leader_id"], name: "index_subscriptions_on_leader_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,6 @@ ActiveRecord::Schema.define(version: 2020_01_18_190223) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "subscriptions", "followers"
+  add_foreign_key "subscriptions", "leaders"
 end
